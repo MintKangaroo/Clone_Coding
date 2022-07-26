c3097b33210152cfb3b1ffb77789b14fdcecef8d
MintKangaroo <yjun273@gmail.com>
2022-07-27 01:11:17 +0900

    프로젝트 다시 세팅 + Navigation 구현

---
+++ KaKaoBank/src/Style/style.tsx
@@ -0,0 +1,16 @@
+import {StyleSheet} from 'react-native';
+
+const style = StyleSheet.create({
+  spash_style: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  main_style: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export default style;
