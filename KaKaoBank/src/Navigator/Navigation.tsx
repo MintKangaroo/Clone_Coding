aa404f64daa7737a5b01d1e6812b4ae4221a1684
MintKangaroo <yjun273@gmail.com>
2022-08-02 00:55:12 +0900

    MainPage Coding & Navigator 상단에 Title Resolved

--- KaKaoBank/src/Navigator/Navigation.tsx
+++ KaKaoBank/src/Navigator/Navigation.tsx
@@ -6,6 +6,8 @@ import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
 import Main from '../Screen/Main';
 import Setting from '../Screen/Setting';
+import Menu from '../Screen/Menu';
+import Notification from '../Screen/Notification';
 
 const Tab = createBottomTabNavigator();
 
@@ -17,16 +19,39 @@ export default () => {
           name="Main"
           component={Main}
           options={{
+            headerShown: false,
             tabBarLabel: '메인',
+            //TODO: Icon 넣기
+          }}
+        />
+        <Tab.Screen
+          name="Menu"
+          component={Menu}
+          options={{
+            headerShown: false,
+            tabBarLabel: '메뉴',
+            //TODO: Icon 넣기
+          }}
+        />
+        <Tab.Screen
+          name="Notification"
+          component={Notification}
+          options={{
+            headerShown: false,
+            tabBarLabel: '알림',
+            //TODO: Icon 넣기
           }}
         />
         <Tab.Screen
           name="Setting"
           component={Setting}
           options={{
+            headerShown: false,
             tabBarLabel: '설정',
+            //TODO: Icon 넣기
           }}
         />
+        {/*TODO: 새로운 Screen 추가될 때 여기에 Route 입력*/}
       </Tab.Navigator>
     </NavigationContainer>
   );
--- KaKaoBank/src/Screen/Main.tsx
+++ KaKaoBank/src/Screen/Main.tsx
@@ -1,10 +1,52 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
 
 export default ({navigation}: any) => {
   return (
-    <View>
-      <Text>Main</Text>
+    <View style={{flex: 1}}>
+      <View
+        style={{
+          marginTop: 10,
+          marginBottom: 10,
+          flex: 1,
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+        }}>
+        <View style={{flexDirection: 'row'}}>
+          <Text style={main_page.top_text}>지창</Text>
+          <TouchableOpacity style={main_page.my_account}>
+            <Text>내 계좌</Text>
+          </TouchableOpacity>
+        </View>
+        <View>
+          <TouchableOpacity style={main_page.profile_custom}></TouchableOpacity>
+        </View>
+      </View>
+      <View style={{flex: 9}}>
+        <Text>Hello</Text>
+      </View>
     </View>
   );
 };
+
+const main_page = StyleSheet.create({
+  top_text: {
+    fontSize: 25,
+    marginRight: 10,
+  },
+  my_account: {
+    width: 55,
+    height: 35,
+    borderRadius: 30,
+    backgroundColor: 'grey',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  profile_custom: {
+    width: 35,
+    height: 35,
+    backgroundColor: 'grey',
+    borderRadius: 35,
+    marginRight: 10,
+  },
+});
